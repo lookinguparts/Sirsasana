2f93e10ab4e36d5cd6aa3e8e1b289c47abf44952
Tracy Scott <tracy.scott@gmail.com>
2022-07-22 18:46:57 -0700

    Basic selector pattern for each of the light sections.

---
+++ src/main/java/art/lookingup/sirsasana/patterns/Selector.java
@@ -0,0 +1,60 @@
+package art.lookingup.sirsasana.patterns;
+
+import art.lookingup.sirsasana.SirsasanaModel;
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.color.ColorParameter;
+import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.pattern.LXPattern;
+
+@LXCategory(LXCategory.FORM)
+public class Selector extends LXPattern {
+
+  static public String[] sections = {
+      "Spike",
+      "Upper",
+      "Lower Up",
+      "Lower Down",
+      "Ground"
+  };
+
+  DiscreteParameter which = new DiscreteParameter("section", sections);
+  ColorParameter color = new ColorParameter("color");
+
+  public Selector(LX lx) {
+    super(lx);
+    addParameter(which);
+    addParameter(color);
+  }
+
+
+  public void run(double deltaMs) {
+    for (LXPoint p : SirsasanaModel.allPoints) {
+      colors[p.index] = LXColor.BLACK;
+    }
+    switch (which.getValuei()) {
+      case 0:
+        for (LXPoint p : SirsasanaModel.topSpineSpikeLights)
+          colors[p.index] = color.getColor();
+        break;
+      case 1:
+        for (LXPoint p : SirsasanaModel.upperRingFloods)
+          colors[p.index] = color.getColor();
+        break;
+      case 2:
+        for (LXPoint p : SirsasanaModel.lowerRingUpFloods)
+          colors[p.index] = color.getColor();
+        break;
+      case 3:
+        for (LXPoint p : SirsasanaModel.lowerRingDownFloods)
+          colors[p.index] = color.getColor();
+        break;
+      case 4:
+        for (LXPoint p : SirsasanaModel.baseFloods)
+          colors[p.index] = color.getColor();
+        break;
+    }
+  }
+}
