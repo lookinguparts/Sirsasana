8183a43de0c04a0f4e9cceed7463ead8c8703ec5
nihar <niharmadhavan@gmail.com>
2022-08-02 01:45:06 -0700

    Add simple bird-glow

---
+++ src/main/java/art/lookingup/sirsasana/patterns/BirdGlow.java
@@ -0,0 +1,39 @@
+package art.lookingup.sirsasana.patterns;
+
+import art.lookingup.sirsasana.SirsasanaModel;
+import heronarts.lx.LX;
+import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.pattern.LXPattern;
+
+public class BirdGlow extends LXPattern {
+
+  int currentIndex = 0;
+  double timePerBirdMs = 2000;
+  double currentElapsed = 0;
+
+  public BirdGlow(LX lx) {
+    super(lx);
+  }
+
+  public void run(double deltaMs) {
+    if (currentElapsed > timePerBirdMs) {
+      currentIndex++;
+      currentElapsed = 0.0;
+      if (currentIndex >= SirsasanaModel.birds.size())
+        currentIndex = 0;
+    }
+    for (SirsasanaModel.Bird bird : SirsasanaModel.birds) {
+      if (bird.id == currentIndex) {
+        for (LXPoint point : bird.points) {
+          colors[point.index] = LXColor.WHITE;
+        }
+      } else {
+        for (LXPoint point : bird.points) {
+          colors[point.index] = LXColor.BLACK;
+        }
+      }
+      currentElapsed += deltaMs;
+    }
+  }
+}
