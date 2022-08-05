3a6aa951dcced80f6aed7f402a12783795d28919
Tracy Scott <tracy.scott@gmail.com>
2022-08-05 16:21:18 -0700

    Autoplay FL Studio with multichannel audio + midi
    Rough draft testing various things.

---
+++ autoplayflstudio.cmd
@@ -0,0 +1,2 @@
+start "C:\\Program Files (x86)\\Image-Line\\FL Studio 20\\FL64.exe" "C:\\Users\\Tracy\\Documents\\GitHub\\sirsasana\\audioroute.flp"
+cscript C:\Users\Tracy\Documents\GitHub\sirsasana\autoplayflstudio.js
---
+++ autoplayflstudio.js
@@ -0,0 +1,5 @@
+var wait = 3000;
+
+var Shell = WScript.CreateObject("WScript.Shell");
+WScript.Sleep(wait);
+Shell.SendKeys(" ");
