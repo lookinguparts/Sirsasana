6b9a79f220ddad67ec14eb770a6bd46e41aebc4a
Tracy Scott <tracy.scott@gmail.com>
2022-08-17 00:30:05 -0700

    Pattern that generates audio.
    This allows for different sonic scenes to be set.

--- src/main/java/art/lookingup/sirsasana/BirdSongGen.java
+++ src/main/java/art/lookingup/sirsasana/BirdSongGen.java
@@ -9,44 +9,8 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class BirdSongGen implements LXLoopTask {
 
-  static public long minSingingPause = 30;  // in seconds;
-  static public long minFrogPause = 30; // in seconds
-  static public float perBirdVolume = 0.09f;
-  static public float randomStartDelay = 30f;
-  static public float randomFrogStartDelay = 30f;
-  static public float frogVolume = 0.09f;
-
   public void loop(double deltaMs) {
-    long now = System.currentTimeMillis();
-    for (Bird bird : SirsasanaModel.birds) {
-      if (now - bird.lastSinging > minSingingPause * 1000 && !bird.waitingToPlay) {
-        // Eligible to pick next start time.
-        bird.startPlayingAt = now + (int)(Math.random() * randomStartDelay * 1000f);
-        bird.waitingToPlay = true;
-        bird.playing = false;
-      } else if (now > bird.startPlayingAt && !bird.playing) {
-        int channel = remapStereo(bird.id/2);
-        float volume = remapStereoVolume(perBirdVolume);
-        Synth.playBird("tracs", bird.id + 1, channel, volume, 2);
-        bird.lastSinging = now;
-        bird.waitingToPlay = false;
-        bird.playing = true;
-      }
-    }
-
-    // Maybe play the frog.
-    if (now - Frog.lastSinging > minFrogPause * 1000 && !Frog.waitingToPlay) {
-      Frog.startPlayingAt = now + (int)(Math.random() * randomFrogStartDelay);
-      Frog.waitingToPlay = true;
-      Frog.playing = false;
-    } else if (now > Frog.startPlayingAt && !Frog.playing) {
-      int channel = ThreadLocalRandom.current().nextInt(0, 6);
-      channel = remapStereo(channel);
-      Synth.playSynth("tracs", "frog", channel, frogVolume);
-      Frog.lastSinging = now;
-      Frog.waitingToPlay = false;
-      Frog.playing = true;
-    }
+    // These are now implemented as individual LXPatterns.
   }
 
   static public float remapStereoVolume(float volume) {
---
+++ src/main/java/art/lookingup/sirsasana/patterns/AudioTracs.java
@@ -0,0 +1,66 @@
+package art.lookingup.sirsasana.patterns;
+
+import art.lookingup.sirsasana.*;
+import heronarts.lx.LX;
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.pattern.LXPattern;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Pattern that just generates audio.
+ *
+ * Notes: Frog play time is < 8 seconds.
+ */
+public class AudioTracs extends LXPattern {
+  DiscreteParameter minSingingPause = new DiscreteParameter("sngPaus", 30, 0, 300).setDescription("Minimum time between bird re-singing");
+  DiscreteParameter minFrogPause = new DiscreteParameter("minFgPaus", 30, 0, 3600).setDescription("Minimum time between frog songs");
+  CompoundParameter perBirdVolume = new CompoundParameter("bVol", 0.09, 0.00, 0.2).setDescription("Bird volume");
+  DiscreteParameter randomStartDelay = new DiscreteParameter("bRndStart", 30, 0, 300).setDescription("Random start delay for birds");
+  DiscreteParameter randomFrogStartDelay = new DiscreteParameter("fRndStart", 30, 0, 3600).setDescription("Random start delay for frog");
+  CompoundParameter frogVolume = new CompoundParameter("fVol", 0.09, 0f, .2).setDescription("Frog volume");
+
+  public AudioTracs(LX lx) {
+    super(lx);
+    addParameter("sngPaus", minSingingPause);
+    addParameter("minFgPaus", minFrogPause);
+    addParameter("bVol", perBirdVolume);
+    addParameter("bRndStart", randomStartDelay);
+    addParameter("fRndStart", randomFrogStartDelay);
+    addParameter("fVol", frogVolume);
+  }
+
+  public void run(double deltaMs) {
+    long now = System.currentTimeMillis();
+    for (Bird bird : SirsasanaModel.birds) {
+      if (now - bird.lastSinging > minSingingPause.getValuei() * 1000 && !bird.waitingToPlay) {
+        // Eligible to pick next start time.
+        bird.startPlayingAt = now + (int)(Math.random() * randomStartDelay.getValuei() * 1000f);
+        bird.waitingToPlay = true;
+        bird.playing = false;
+      } else if (now > bird.startPlayingAt && !bird.playing) {
+        int channel = BirdSongGen.remapStereo(bird.id/2);
+        float volume = BirdSongGen.remapStereoVolume(perBirdVolume.getValuef());
+        Synth.playBird("tracs", bird.id + 1, channel, volume, 2);
+        bird.lastSinging = now;
+        bird.waitingToPlay = false;
+        bird.playing = true;
+      }
+    }
+
+    // Maybe play the frog.
+    if (now - Frog.lastSinging > minFrogPause.getValuei() * 1000 && !Frog.waitingToPlay) {
+      Frog.startPlayingAt = now + (int)(Math.random() * randomFrogStartDelay.getValuei());
+      Frog.waitingToPlay = true;
+      Frog.playing = false;
+    } else if (now > Frog.startPlayingAt && !Frog.playing) {
+      int channel = ThreadLocalRandom.current().nextInt(0, 6);
+      channel = BirdSongGen.remapStereo(channel);
+      Synth.playSynth("tracs", "frog", channel, frogVolume.getValuef());
+      Frog.lastSinging = now;
+      Frog.waitingToPlay = false;
+      Frog.playing = true;
+    }
+  }
+}
