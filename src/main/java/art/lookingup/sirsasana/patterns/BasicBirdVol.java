40751a1853f756c57adbecf443884d5ea72c6c6e
Tracy Scott <tracy.scott@gmail.com>
2022-08-17 14:37:07 -0700

    More BasicBirdVol functionality.
    A choice of 3 colors and birds are randomly assigned or 3 palettes and birds are randomly assigned.

--- src/main/java/art/lookingup/sirsasana/patterns/BasicBirdVol.java
+++ src/main/java/art/lookingup/sirsasana/patterns/BasicBirdVol.java
@@ -3,6 +3,7 @@ package art.lookingup.sirsasana.patterns;
 import art.lookingup.EaseUtil;
 import art.lookingup.colors.Colors;
 import art.lookingup.sirsasana.Bird;
+import art.lookingup.sirsasana.SirsasanaModel;
 import heronarts.lx.LX;
 import heronarts.lx.color.ColorParameter;
 import heronarts.lx.color.LXColor;
@@ -12,18 +13,27 @@ import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.DiscreteParameter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class BasicBirdVol extends BirdVol {
 
   ColorParameter color = new ColorParameter("color");
+  ColorParameter color2 = new ColorParameter("color2");
+  ColorParameter color3 = new ColorParameter("color3");
   BooleanParameter usePal = new BooleanParameter("usePal", false);
   CompoundParameter bgintensity = new CompoundParameter("bgi", 0, 0, 1 ).setDescription("Background Intensity");
   CompoundParameter maxIntensity = new CompoundParameter("maxi", 1f, 0f, 1f).setDescription("Max intensity");
   CompoundParameter sparkle = new CompoundParameter("sparkle", 0,0,1).setDescription("Sparkle Effect");
   DiscreteParameter easeParam = new DiscreteParameter("ease", 0, 0, EaseUtil.MAX_EASE + 1);
   DiscreteParameter swatch = new DiscreteParameter("swatch", 0, 0, 20);
-  CompoundParameter perlinFreq = new CompoundParameter("perlFreq", 1f, 0f, 20f);
+  DiscreteParameter swatch2 = new DiscreteParameter("swatch2", 0, 0, 20);
+  DiscreteParameter swatch3 = new DiscreteParameter("swatch3", 0, 0, 20);
+  CompoundParameter perlinFreq = new CompoundParameter("perlFreq", 1f, 0f, 40f);
   CompoundParameter idleIntensity = new CompoundParameter("idlei", 0.2f, 0f, 1f).setDescription("Idle intensity");
   CompoundParameter volScale = new CompoundParameter("volScl", 1f, 0f, 5f).setDescription("Volume feedback scale");
+  CompoundParameter idleFreq = new CompoundParameter("idlfreq", 1f, 0f, 5f).setDescription("Freq when idle");
 
   double totalSecs = 0f;
   EaseUtil ease = new EaseUtil(0);
@@ -32,22 +42,36 @@ public class BasicBirdVol extends BirdVol {
 
     super(lx);
     addParameter("color", color);
+    addParameter("color2", color2);
+    addParameter("color3", color3);
     addParameter("bgi", bgintensity);
     addParameter("maxi", maxIntensity);
     addParameter("sparkle", sparkle);
     addParameter("usePal", usePal);
     addParameter("ease", easeParam);
     addParameter("swatch", swatch);
+    addParameter("swatch2", swatch2);
+    addParameter("swatch3", swatch3);
     addParameter("perlFreq", perlinFreq);
     addParameter("idlei", idleIntensity);
     addParameter("volScale", volScale);
+    addParameter("idlfreq", idleFreq);
     color.brightness.setValue(100);
+    color2.brightness.setValue(100);
+    color3.brightness.setValue(100);
 
   }
+
+  Map<Bird, Integer> birdSwatchMap = new HashMap<Bird, Integer>();
 
   @Override
   public void onActive() {
     totalSecs = 0f;
+    // map birds to which color/swatch they will use.
+    birdSwatchMap.clear();
+    for (Bird bird: SirsasanaModel.birds) {
+      birdSwatchMap.put(bird, ThreadLocalRandom.current().nextInt(0, 3));
+    }
   }
 
   @Override
@@ -74,15 +98,37 @@ public class BasicBirdVol extends BirdVol {
   }
 
   public int getColor(LXPoint p, Bird b) {
-    return getColor(p, ease.ease(pointT(p, b)));
+    return getColor(p, ease.ease(pointT(p, b)), b);
   }
 
-  public int getColor(LXPoint p, float t) {
-    int clr = color.getColor();
+  public int getColor(LXPoint p, float t, Bird b) {
+    int clr = LXColor.BLACK;
+    int which = birdSwatchMap.get(b);
+    switch (which) {
+      case 0:
+        clr = color.getColor();
+        break;
+      case 1:
+        clr = color2.getColor();
+        break;
+      case 2:
+        clr = color3.getColor();
+        break;
+    }
     clr = Colors.getWeightedColor(clr, ease.ease(t));
 
     if (usePal.getValueb()) {
-      clr = Colors.getParameterizedPaletteColor(lx, swatch.getValuei(), t, ease);
+      switch (which) {
+        case 0:
+          clr = Colors.getParameterizedPaletteColor(lx, swatch.getValuei(), t, ease);
+          break;
+        case 1:
+          clr = Colors.getParameterizedPaletteColor(lx, swatch2.getValuei(), t, ease);
+          break;
+        case 2:
+          clr = Colors.getParameterizedPaletteColor(lx, swatch3.getValuei(), t, ease);
+          break;
+      }
     }
     return clr;
   }
@@ -100,9 +146,9 @@ public class BasicBirdVol extends BirdVol {
   public void renderBirdIdle(int[] colors, Bird bird, double deltaMs) {
     ease.easeNum = easeParam.getValuei();
     if (ease.easeNum == 8) {
-      ease.perlinFreq = 1f;
+      ease.perlinFreq = idleFreq.getValuef();
     } else if (ease.easeNum == 6) {
-      ease.freq = 1f;
+      ease.freq = idleFreq.getValuef();
     }
     for (LXPoint p : bird.points) {
       int clr = getColor(p, bird);
